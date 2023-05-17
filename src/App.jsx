6eba27c8800d4e8855b4ecd2d57d8945427bdb68
Will-Swinson <william.swinson01@gmail.com>
2023-05-17 18:45:34 -0500

    sdada

--- src/App.jsx
+++ src/App.jsx
@@ -60,13 +60,62 @@ import Login from "./Login";
 
 const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=6149eda588f347a0856c12deaaff09a3&response_type=code&redirect_uri=https://soundstream-q4fk.onrender.com&scope=ugc-image-upload%20user-read-playback-state%20user-modify-playback-state%20user-read-currently-playing%20streaming%20playlist-read-private%20playlist-modify-private%20playlist-modify-public%20user-top-read%20user-read-recently-played%20user-library-modify%20user-library-read`;
 
+// function App() {
+//   const [currentUser, setCurrentUser] = useState(null);
+
+//   const code = new URLSearchParams(window.location.search).get("code");
+
+//   const handleSubmit = async (email, username, password) => {
+//     try {
+//       console.log(email, username, password);
+//       const newUser = { username, password, email };
+
+//       const response = await axios.post(
+//         "https://soundstream-api.onrender.com/api/users",
+//         newUser,
+//         {
+//           headers: {
+//             "Content-Type": "application/json",
+//           },
+//         }
+//       );
+
+//       // console.log(response.data);
+//       let selectedUserId = response.data.selectedUser.id;
+
+//       localStorage.setItem("selectedUserId", selectedUserId);
+
+//       setCurrentUser(selectedUserId);
+//       window.location.href = AUTH_URL;
+//     } catch (err) {
+//       console.error(err);
+//     }
+//   };
+
+//   useEffect(() => {
+//     let selectedUserId = localStorage.getItem("selectedUserId");
+//     setCurrentUser(selectedUserId);
+//   }, []);
+
+//   if (currentUser && code) {
+//     return <Dashboard code={code} />;
+//   } else {
+//     return <Login handleSubmit={handleSubmit} />;
+//   }
+// }
+
 function App() {
   const [currentUser, setCurrentUser] = useState(null);
-
   const code = new URLSearchParams(window.location.search).get("code");
+
+  useEffect(() => {
+    let selectedUserId = localStorage.getItem("selectedUserId");
+    setCurrentUser(selectedUserId);
+  }, [code]);
 
   const handleSubmit = async (email, username, password) => {
     try {
+      window.location.href = AUTH_URL;
       console.log(email, username, password);
       const newUser = { username, password, email };
 
@@ -80,24 +129,18 @@ function App() {
         }
       );
 
-      // console.log(response.data);
+      console.log(response.data);
       let selectedUserId = response.data.selectedUser.id;
 
       localStorage.setItem("selectedUserId", selectedUserId);
 
       setCurrentUser(selectedUserId);
-      window.location.href = AUTH_URL;
     } catch (err) {
       console.error(err);
     }
   };
 
-  useEffect(() => {
-    let selectedUserId = localStorage.getItem("selectedUserId");
-    setCurrentUser(selectedUserId);
-  }, []);
-
-  if (currentUser && code) {
+  if (currentUser || code) {
     return <Dashboard code={code} />;
   } else {
     return <Login handleSubmit={handleSubmit} />;
