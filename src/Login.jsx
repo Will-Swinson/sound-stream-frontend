716bbf2597ffae82767e5f50e9758a4e71a2caa3
Will-Swinson <william.swinson01@gmail.com>
2023-05-17 10:19:43 -0500

    asd

--- src/Login.jsx
+++ src/Login.jsx
@@ -162,6 +162,7 @@ export default function Login() {
     }
   };
 
+  console.log(currentUser);
   return (
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
